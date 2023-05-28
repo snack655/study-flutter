c927365e081ac8a3cdc136eff14b51f68bfc1232
최민재 <cmjcmj1230@gmail.com>
2023-05-28 16:38:24 +0900

    Add Header wallet app

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(App());
@@ -7,17 +8,48 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          elevation: 10,
-          title: Text("Hello Flutter!"),
-        ),
-        body: Center(
-          child: Text("Hello world!"),
-        ),
-      ),
-    );
+    return const MaterialApp(
+        home: Scaffold(
+      backgroundColor: Color(0xFF181818),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Hey, Selena',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 34,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                          Opacity(
+                            opacity: 0.8,
+                            child: Text(
+                              'Welcome back',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ))),
+    ));
   }
 }
