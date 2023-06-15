1c3bd16d9fa8a31ad6fccab692e714daa70827a9
최민재 <cmjcmj1230@gmail.com>
2023-06-15 11:26:53 +0900

    Fix State to setState

--- toonflix/lib/test.dart
+++ toonflix/lib/test.dart
@@ -11,7 +11,9 @@ class _MyAppState extends State<MyApp> {
   int counter = 0;
 
   void onClicked() {
-    counter = counter + 1;
+    setState(() {
+      counter = counter + 1;
+    });
   }
 
   @override
