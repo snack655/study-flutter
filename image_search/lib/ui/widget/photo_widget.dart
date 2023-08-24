dc4e8597cc2b30546aa29be8d3b4f898af5f3b3b
최민재 <cmjcmj1230@gmail.com>
2023-08-24 09:44:38 +0900

    photo app

---
+++ image_search/lib/ui/widget/photo_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/model/photo.dart';
+
+class PhotoWidget extends StatelessWidget {
+  final Photo photo;
+
+  const PhotoWidget({
+    super.key,
+    required this.photo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+            photo.webformatURL,
+          ),
+        ),
+      ),
+    );
+  }
+}
